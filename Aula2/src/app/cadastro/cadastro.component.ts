1693447e69bdef923e1f8e4b0a6748cdd0d4d191
TREINA\gravacao <alicio.fcn@gmail.com>
2021-07-03 17:01:16 -0300

    Consultar objetos e listar em tabela

--- Aula2/src/app/cadastro/cadastro.component.ts
+++ Aula2/src/app/cadastro/cadastro.component.ts
@@ -10,5 +10,29 @@ export class CadastroComponent  {
    getValues(data:any){
      console.warn(data);
   }
+  
+  contatos = [
+    {
+      nome:'Vilson',
+      idade: 45,
+      email: 'vilson@gmail.com'
+    }
+    ,
+    {
+      nome:'Ana',
+      idade: 15,
+      email: 'ana@gmail.com'
+    },
+    {
+      nome:'Pedro',
+      idade: 35,
+      email: 'pedro@gmail.com'
+    },
+    {
+      nome:'Antonio',
+      idade: 55,
+      email: 'antonio@gmail.com'
+    }
+  ]
 
 }
